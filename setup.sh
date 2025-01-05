2f40e8705c6d5cd3e8f2fc125b2d0e257a20bd9a
duong <dotungduong2@gmail.com>
2025-01-05 23:18:31 +0700

    cleanup
    cleanup unused code

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Get current working directory
-#old_dir=$(pwd -P)
-
-# Change to directory of script
-#cd $(dirname $0)
-
 # Install dependencies
 sudo -k apt install python3-venv
 pip3 install virtualenvwrapper
@@ -20,7 +14,3 @@ echo $'#!/bin/bash\nsource $(which virtualenvwrapper.sh)\nworkon youtube-downloa
 echo "python3 $(dirname $(realpath "$0"))/youtube-downloader" >> $HOME/.local/bin/youtube-downloader
 echo 'deactivate' >> $HOME/.local/bin/youtube-downloader
 chmod +x $HOME/.local/bin/youtube-downloader
-
-
-# Change to old directory
-#cd $old_dirt
