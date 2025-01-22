89d09030948024525131d203c141fee618309610
duong <dotungduong2@gmail.com>
2025-01-22 18:02:23 +0700

    switch to setuptools

--- youtube_downloader/__init__.py
+++ youtube_downloader/__init__.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 
 scriptDir = Path(__file__).parent
-if (p := scriptDir.joinpath("VERSION")).exists():
-    __version__ = p.read_text()
-else:
-    __version__ = scriptDir.parent.joinpath("VERSION").read_text()
+
+__version__ = scriptDir.joinpath("VERSION").read_text()
 
 __all__ = ["__version__"]
